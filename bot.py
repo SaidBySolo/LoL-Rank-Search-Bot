afc28f4f66d4c59f498dc34716edf4e1d2cf69f1
SaidBySolo <saidbysolo@gmail.com>
2020-04-21 13:02:17 +0900

    Edit:raw string

--- bot.py
+++ bot.py
@@ -2,19 +2,37 @@ import discord
 from discord.ext import commands
 import json
 
-with open("cogs\etc\Auth.json", "r") as Auth:
+with open(r"cogs\etc\Auth.json", "r") as Auth:
     loadjson = json.load(Auth)
 
 token = loadjson['DiscordToken']
 
-bot = commands.Bot(command_prefix="$")
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix="&")
 
-extensions = ['cogs.status',
-            'cogs.solo5x5',
-            'cogs.flexsr',
-            'cogs.events']
+def load_cogs(bot):
+    extensions = ['cogs.status',
+                'cogs.solo5x5',
+                'cogs.flexsr',
+                'cogs.events']
 
-for extention in extensions:
-    bot.load_extension(extention)
 
-bot.run(token)
+    failed = []
+
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f"{e.__class__.__name__}: {str(e)}")
+            failed.append(extension)
+
+    if failed:
+        print(f"\n{' '.join(failed)}를 로드하는데 실패했습니다.\n")
+    return failed
+    
+
+if __name__ == '__main__':
+    bot = Bot()
+    load_cogs(bot)
+    bot.run(token)
--- cogs/etc/riot.py
+++ cogs/etc/riot.py
@@ -2,7 +2,7 @@ from riotwatcher import LolWatcher
 import json
 
 def watcher():
-    with open("cogs\etc\Auth.json", "r") as Auth:
+    with open(r"cogs\etc\Auth.json", "r") as Auth:
         token = json.load(Auth)
         watcher = LolWatcher(token['RiotAPIToken'])
         return watcher
